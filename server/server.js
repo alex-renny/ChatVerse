04de93cc41cbc345c23d9c4bef580e8dab588c10
alex-renny <mr.alex112524@gmail.com>
2026-07-04 00:11:25 +0530

    Complete backend authentication with JWT

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import authRoutes from "./routes/authRoutes.js";
 
 import connectDB from "./config/db.js";
 
@@ -12,6 +13,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use("/api/auth", authRoutes);
 
 app.get("/", (req, res) => {
   res.send("🚀 ChatVerse API Running");
